f991f228fa6475813c69edcff0e66a9f660e31ec
Tetsuharu OHZEKI <saneyuki.snyk@gmail.com>
2016-11-14 16:44:55 +0900

    implement generate_uuid_v4()

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,12 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-    }
+extern crate uuid;
+
+use std::ffi::CString;
+use uuid::Uuid;
+
+#[no_mangle]
+pub extern fn generate_uuid_v4() -> CString {
+    let uuid = Uuid::new_v4();
+    let s = uuid.hyphenated().to_string();
+    let cstr = CString::new(s).unwrap();
+    cstr
 }
