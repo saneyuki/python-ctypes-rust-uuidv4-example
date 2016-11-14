32c3f7287ac9e9cf6ae9b5f1d866ad5e588dcef9
Tetsuharu OHZEKI <saneyuki.snyk@gmail.com>
2016-11-14 17:23:40 +0900

    Return explicit typed raw pointer.

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,14 @@
 extern crate uuid;
 
-use std::ffi::CString;
 use uuid::Uuid;
 
+use std::ffi::CString;
+use std::os::raw::c_char;
+
 #[no_mangle]
-pub extern fn generate_uuid_v4() -> CString {
+pub extern fn generate_uuid_v4() -> *const c_char {
     let uuid = Uuid::new_v4();
     let s = uuid.hyphenated().to_string();
     let cstr = CString::new(s).unwrap();
-    cstr
+    cstr.into_raw()
 }
